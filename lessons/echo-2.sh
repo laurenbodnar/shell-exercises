dfb56ea6e047d094feb7cc02d2a9e5fb5a8f1ff6
Lauren Bodnar <lauren.n.bodnar@gmail.com>
2018-09-17 21:36:21 -0500

    Merged lesson2 & 3 folders into single directory and change names of files for clarity

---
+++ lessons/echo-2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Following exercise @ https://www.hackerrank.com/challenges/bash-tutorials---a-personalized-echo/
+
+# Challenge: Write a Bash script which accepts name  as input and displays a greeting: "Welcome (name)"
+
+echo "Welcome $1"
